880392b0a40e64a60277e62c0a8b272a08f86172
webstandupovka-netizen <webstandupovka.netizen@gmail.com>
2025-08-22 12:58:19 +0300

    Clean up project: remove unnecessary test files, migration scripts, and documentation

--- add-user-free-access.sql
+++
@@ -1,8 +0,0 @@
--- Add free_access field to user_profiles table
-ALTER TABLE user_profiles ADD COLUMN IF NOT EXISTS free_access BOOLEAN DEFAULT FALSE;
-
--- Update existing users to have free_access = false by default
-UPDATE user_profiles SET free_access = FALSE WHERE free_access IS NULL;
-
--- Make the column NOT NULL
-ALTER TABLE user_profiles ALTER COLUMN free_access SET NOT NULL;
